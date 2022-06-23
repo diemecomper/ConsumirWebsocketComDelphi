d3020bab5fd76a7d638d028772f3ada25b033947
diemecomper <diemecomper@gmail.com>
2022-06-23 08:42:22 -0300

    Update UWebSocket.pas

--- UWebSocket.pas
+++ UWebSocket.pas
@@ -5,8 +5,7 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, IdBaseComponent,
-  IdComponent, IdTCPConnection, IdTCPClient, IdEcho, IdHTTP,
-  Bird.Socket.Client,
+  IdComponent, IdTCPConnection, IdTCPClient, IdEcho, IdHTTP
   IdWebSocketSimpleClient,
   System.JSON, System.Win.ScktComp, Vcl.ExtCtrls;
 
